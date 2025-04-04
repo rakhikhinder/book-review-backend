dda08fcb171f1a8ce9f968442667a1890d607abe
rakhikhinder <rakhikhinder@yahoo.com>
2025-04-04 20:39:46 +0530

    review implementation done

--- models/review.py
+++ models/review.py
@@ -2,8 +2,9 @@ from utils.db import db
 from bson import ObjectId
 
 class Review:
-    def __init__(self, user_id, title, message):
+    def __init__(self, user_id, book_id, title, message):
         self.user_id = user_id
+        self.book_id = book_id
         self.title = title
         self.message = message
 
@@ -22,3 +23,9 @@ class Review:
     @staticmethod
     def delete(review_id):
         return db.reviews.delete_one({"_id": ObjectId(review_id)})
+
+    @staticmethod
+    def get_by_user_and_book(user_id, book_id):
+        return list(db.reviews.find({"user_id": user_id, "book_id": book_id}))
+
+

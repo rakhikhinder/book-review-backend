bc0d910f39e99862375a9a74b024f7f5920279b0
rakhikhinder <rakhikhinder@yahoo.com>
2025-04-05 13:48:48 +0530

    Cors added and port number updated

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from routes.book_routes import book_routes
 from routes.user_routes import user_routes
 from routes.review_routes import review_routes
 from config import Config
-
+from flask_cors import CORS
 app = Flask(__name__)
+CORS(app) 
 app.register_blueprint(book_routes, url_prefix='/api')
 app.register_blueprint(user_routes, url_prefix='/api')
 app.register_blueprint(review_routes, url_prefix='/api')
@@ -14,4 +15,4 @@ def home():
     return "Welcome to the Bookstore API"
 
 if __name__ == "__main__":
-    app.run(debug=True,port=Config.PORT)
+    app.run(port=Config.PORT)
